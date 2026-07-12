2db5dd23255bc3e2296b70723d21126658e041d3
Jake Koperski <wijakoperski@gmail.com>
2026-07-12 11:42:53 -0500

    Refactor ScoreLogger to handle structured log rather than strings

--- src/components/ScoreLogger.jsx
+++ src/components/ScoreLogger.jsx
@@ -1,9 +1,63 @@
+function stringifyCard(card) {
+  const { rank, suit } = card;
+  const rankString =
+    rank === 14
+      ? "A"
+      : rank === 13
+        ? "K"
+        : rank === 12
+          ? "Q"
+          : rank === 11
+            ? "J"
+            : rank.toString();
+  const suitString =
+    suit === "Hearts"
+      ? "H"
+      : suit === "Diamonds"
+        ? "D"
+        : suit === "Clubs"
+          ? "C"
+          : suit === "Spades"
+            ? "S"
+            : "";
+  return `${rankString}${suitString}`;
+}
+
+function stringifyHandPlayed(entry) {
+  const { hand, baseChips, baseMult, scoredCards } = entry;
+  return `${hand} | ${baseChips} × ${baseMult} | ${scoredCards.map(stringifyCard)}`;
+}
+
+function stringifyCardScored(entry) {
+  const { card, addChips } = entry;
+  return `${stringifyCard(card)} | +${addChips}c`;
+}
+
+function stringifyJokerScored(entry) {
+  const { joker, addChips, addMult, multMult } = entry;
+
+  const operations = [];
+  if (addChips) operations.push(`+${addChips}c`);
+  if (addMult) operations.push(`+${addMult}m`);
+  if (multMult) operations.push(`+×${multMult}m`);
+
+  return `${joker} | ${operations.join(", ")}`;
+}
+
 function ScoreLogger({ log }) {
   return (
     <div className="score-logger">
-      {log.map((entry, index) => (
-        <code key={index}>{entry}</code>
-      ))}
+      {log.length === 0 && <code>No Hand</code>}
+      {log.map((entry, index) => {
+        switch (entry.type) {
+          case "HAND_PLAYED":
+            return <code>{stringifyHandPlayed(entry)}</code>;
+          case "CARD_SCORED":
+            return <code>{stringifyCardScored(entry)}</code>;
+          case "JOKER_SCORED":
+            return <code>{stringifyJokerScored(entry)}</code>;
+        }
+      })}
     </div>
   );
 }
--- src/engine/scoreResolver.js
+++ src/engine/scoreResolver.js
@@ -1,58 +1,23 @@
 import { identifyHandPlayed, identifyAllHandsPlayed } from "./handResolver.js";
 
-function stringifyCard(card) {
-  const { rank, suit } = card;
-  const rankString =
-    rank === 14
-      ? "A"
-      : rank === 13
-        ? "K"
-        : rank === 12
-          ? "Q"
-          : rank === 11
-            ? "J"
-            : rank.toString();
-  const suitString =
-    suit === "Hearts"
-      ? "H"
-      : suit === "Diamonds"
-        ? "D"
-        : suit === "Clubs"
-          ? "C"
-          : suit === "Spades"
-            ? "S"
-            : "";
-  return `${rankString}${suitString}`;
-}
-
 function resolveScore(allCards, handMap, allJokers, gameMetadata) {
   const playedCards = allCards.filter((card) => card.rank !== 0);
   const playedJokers = allJokers.filter((joker) => joker !== "None");
 
   const [handPlayed, scoredCards] = identifyHandPlayed(playedCards);
   const allHandsPlayed = identifyAllHandsPlayed(playedCards);
-  if (handPlayed === "No Hand") return [0, 0, ["No Hand"]];
+  if (handPlayed === "No Hand") return [0, 0, []];
 
-  let [chips, mult] = handMap[handPlayed];
+  const [baseChips, baseMult] = handMap[handPlayed];
   const log = [
-    `${handPlayed} | ${chips} × ${mult}`,
-    `Scored: ${scoredCards.map(stringifyCard)}`,
+    {
+      type: "HAND_PLAYED",
+      hand: handPlayed,
+      baseChips,
+      baseMult,
+      scoredCards,
+    },
   ];
-
-  function addChipsOrMultAndLog(header, addedChips, addedMult) {
-    let readout = " | ";
-    if (addedChips > 0 && addedMult > 0) {
-      readout += `+${addedChips}c, +${addedMult}m`;
-    } else if (addedChips > 0) {
-      readout += `+${addedChips}c`;
-    } else if (addedMult > 0) {
-      readout += `+${addedMult}m`;
-    }
-
-    chips += addedChips;
-    mult += addedMult;
-    log.push(header + readout);
-  }
 
   function isFaceCard(card) {
     return (
@@ -61,26 +26,31 @@ function resolveScore(allCards, handMap, allJokers, gameMetadata) {
     );
   }
 
+  function isOddRank(card) {
+    // Rank 14 is an Ace, which is treated instead as rank 1 in this context
+    return [14, 9, 7, 5, 3].includes(card.rank);
+  }
+
   delete gameMetadata.hangingChadRepeats;
 
   // BODY OF ROUND - CYCLE THROUGH PLAYED CARDS
   for (let i = 0; i < scoredCards.length; i++) {
     const card = scoredCards[i];
     const { rank } = card;
-    addChipsOrMultAndLog(stringifyCard(card), rank, 0);
+    log.push({ type: "CARD_SCORED", card, addChips: rank });
 
     for (const joker of playedJokers) {
       if (joker === "Even Steven") {
         if (rank % 2 === 0) {
-          addChipsOrMultAndLog(joker, 0, 4);
+          log.push({ type: "JOKER_SCORED", joker, addMult: 4 });
         }
       } else if (joker === "Hanging Chad") {
         if (i === 0 && gameMetadata.hangingChadRepeats === undefined) {
           gameMetadata.hangingChadRepeats = 2;
         }
       } else if (joker === "Odd Todd") {
-        if (rank < 11 && rank % 2 === 1) {
-          addChipsOrMultAndLog(joker, 0, 31);
+        if (isOddRank(card)) {
+          log.push({ type: "JOKER_SCORED", joker, addChips: 31 });
         }
       } else if (joker === "Photograph") {
         if (isFaceCard(card)) {
@@ -88,24 +58,24 @@ function resolveScore(allCards, handMap, allJokers, gameMetadata) {
             .slice(0, i)
             .every((card) => !isFaceCard(card));
           if (isFirstFaceCard) {
-            addChipsOrMultAndLog(joker, 0, 2);
+            log.push({ type: "JOKER_SCORED", joker, addMult: 2 });
           }
         }
       } else if (joker === "Scary Face") {
         if (isFaceCard(card)) {
-          addChipsOrMultAndLog(joker, 30, 0);
+          log.push({ type: "JOKER_SCORED", joker, addChips: 30 });
         }
       } else if (joker === "Scholar") {
         if (rank === 14) {
-          addChipsOrMultAndLog(joker, 30, 4);
+          log.push({ type: "JOKER_SCORED", joker, addChips: 20, addMult: 4 });
         }
       } else if (joker === "Smiley Face") {
         if (isFaceCard(card)) {
-          addChipsOrMultAndLog(joker, 0, 5);
+          log.push({ type: "JOKER_SCORED", joker, addMult: 5 });
         }
       } else if (joker === "Walkie Talkie") {
         if (rank === 4 || rank === 10) {
-          addChipsOrMultAndLog(joker, 10, 4);
+          log.push({ type: "JOKER_SCORED", joker, addChips: 10, addMult: 4 });
         }
       }
     }
@@ -119,65 +89,75 @@ function resolveScore(allCards, handMap, allJokers, gameMetadata) {
   // END OF ROUND - CYCLE THROUGH JOKERS
   for (const joker of playedJokers) {
     if (joker === "Abstract Joker") {
-      const addedMult = 3 * playedJokers.length;
-      addChipsOrMultAndLog(joker, 0, addedMult);
+      const addMult = 3 * playedJokers.length;
+      log.push({ type: "JOKER_SCORED", joker, addMult });
     } else if (joker === "Banner") {
       if (gameMetadata.remainingDiscards > 0) {
-        const addedChips = 30 * gameMetadata.remainingDiscards;
-        addChipsOrMultAndLog(joker, addedChips, 0);
+        const addChips = 30 * gameMetadata.remainingDiscards;
+        log.push({ type: "JOKER_SCORED", joker, addChips });
       }
     } else if (joker === "Cavendish") {
-      addChipsOrMultAndLog(joker, 0, 3);
+      log.push({ type: "JOKER_SCORED", joker, addMult: 3 });
     } else if (joker === "Clever Joker") {
       if (allHandsPlayed.includes("Two Pair")) {
-        addChipsOrMultAndLog(joker, 80, 0);
+        log.push({ type: "JOKER_SCORED", joker, addChips: 80 });
       }
     } else if (joker === "Crafty Joker") {
       if (allHandsPlayed.includes("Flush")) {
-        addChipsOrMultAndLog(joker, 80, 0);
+        log.push({ type: "JOKER_SCORED", joker, addChips: 80 });
       }
     } else if (joker === "Crazy Joker") {
       if (allHandsPlayed.includes("Straight")) {
-        addChipsOrMultAndLog(joker, 0, 12);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 12 });
       }
     } else if (joker === "Devious Joker") {
       if (allHandsPlayed.includes("Straight")) {
-        addChipsOrMultAndLog(joker, 100, 0);
+        log.push({ type: "JOKER_SCORED", joker, addChips: 100 });
       }
     } else if (joker === "Droll Joker") {
       if (allHandsPlayed.includes("Flush")) {
-        addChipsOrMultAndLog(joker, 0, 10);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 10 });
       }
     } else if (joker === "Gros Michel") {
-      addChipsOrMultAndLog(joker, 0, 15);
+      log.push({ type: "JOKER_SCORED", joker, addMult: 15 });
     } else if (joker === "Half Joker") {
       if (scoredCards.length <= 3) {
-        addChipsOrMultAndLog(joker, 0, 20);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 20 });
       }
     } else if (joker === "Joker") {
-      addChipsOrMultAndLog(joker, 0, 2);
+      log.push({ type: "JOKER_SCORED", joker, addMult: 2 });
     } else if (joker === "Jolly Joker") {
       if (allHandsPlayed.includes("Pair")) {
-        addChipsOrMultAndLog(joker, 0, 8);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 8 });
       }
     } else if (joker === "Mad Joker") {
       if (allHandsPlayed.includes("Two Pair")) {
-        addChipsOrMultAndLog(joker, 0, 10);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 10 });
       }
     } else if (joker === "Sly Joker") {
       if (allHandsPlayed.includes("Pair")) {
-        addChipsOrMultAndLog(joker, 50, 0);
+        log.push({ type: "JOKER_SCORED", joker, addChips: 50 });
       }
     } else if (joker === "Wily Joker") {
       if (allHandsPlayed.includes("Three of a Kind")) {
-        addChipsOrMultAndLog(joker, 100, 0);
+        log.push({ type: "JOKER_SCORED", joker, addChips: 100 });
       }
     } else if (joker === "Zany Joker") {
       if (allHandsPlayed.includes("Three of a Kind")) {
-        addChipsOrMultAndLog(joker, 0, 12);
+        log.push({ type: "JOKER_SCORED", joker, addMult: 12 });
       }
     }
   }
+
+  const [chips, mult] = log.reduce(
+    ([chips, mult], entry) => {
+      return [
+        chips + (entry.addChips || 0),
+        (mult + (entry.addMult || 0)) * (entry.multMult || 1),
+      ];
+    },
+    [0, 0],
+  );
 
   return [chips, mult, log];
 }
