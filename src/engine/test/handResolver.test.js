acfef04c4fc4bae4f878840488cd83ecbdf2b3ba
Jake Koperski <wijakoperski@gmail.com>
2026-07-12 21:08:49 -0500

    Test handIsStraightFlush

--- src/engine/test/handResolver.test.js
+++ src/engine/test/handResolver.test.js
@@ -1,37 +1,5 @@
-import { handIsRoyalFlush } from "../handResolver.js";
-
-function parseCard(cardStr) {
-  const suitMap = {
-    H: "hearts",
-    D: "diamonds",
-    C: "clubs",
-    S: "spades",
-  };
-  const rankMap = {
-    A: 14,
-    K: 13,
-    Q: 12,
-    J: 11,
-    10: 10,
-    9: 9,
-    8: 8,
-    7: 7,
-    6: 6,
-    5: 5,
-    4: 4,
-    3: 3,
-    2: 2,
-  };
-
-  const suit = suitMap[cardStr.slice(-1)];
-  const rank = rankMap[cardStr.slice(0, -1)];
-
-  return { suit, rank };
-}
-
-function parseCards(cardStrs) {
-  return cardStrs.map(parseCard);
-}
+import { handIsRoyalFlush, handIsStraightFlush } from "../handResolver.js";
+import { parseCards } from "./utils.js";
 
 describe("handIsRoyalFlush", () => {
   it("is true when royal flush is present", () => {
@@ -52,5 +20,27 @@ describe("handIsRoyalFlush", () => {
   it("is false when there are not enough cards for royal flush", () => {
     const playedCards = parseCards(["AH", "KH", "QH"]);
     expect(handIsRoyalFlush(playedCards)).toBe(false);
+  });
+});
+
+describe("handIsStraightFlush", () => {
+  it("is true when straight flush is present", () => {
+    const playedCards = parseCards(["9H", "8H", "7H", "6H", "5H"]);
+    expect(handIsStraightFlush(playedCards)).toBe(true);
+  });
+
+  it("is false when ranks are not consecutive", () => {
+    const playedCards = parseCards(["9H", "8H", "7H", "6H", "4H"]);
+    expect(handIsStraightFlush(playedCards)).toBe(false);
+  });
+
+  it("is false when cards are not all the same suit", () => {
+    const playedCards = parseCards(["9H", "8H", "7H", "6H", "5D"]);
+    expect(handIsStraightFlush(playedCards)).toBe(false);
+  });
+
+  it("is false when there are not enough cards for straight flush", () => {
+    const playedCards = parseCards(["9H", "8H", "7H"]);
+    expect(handIsStraightFlush(playedCards)).toBe(false);
   });
 });
---
+++ src/engine/test/utils.js
@@ -0,0 +1,34 @@
+function parseCard(cardStr) {
+  const suitMap = {
+    H: "hearts",
+    D: "diamonds",
+    C: "clubs",
+    S: "spades",
+  };
+  const rankMap = {
+    A: 14,
+    K: 13,
+    Q: 12,
+    J: 11,
+    10: 10,
+    9: 9,
+    8: 8,
+    7: 7,
+    6: 6,
+    5: 5,
+    4: 4,
+    3: 3,
+    2: 2,
+  };
+
+  const suit = suitMap[cardStr.slice(-1)];
+  const rank = rankMap[cardStr.slice(0, -1)];
+
+  return { suit, rank };
+}
+
+function parseCards(cardStrs) {
+  return cardStrs.map(parseCard);
+}
+
+export { parseCard, parseCards };
